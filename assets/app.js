6434eb6c6d8ca3347b806d16281ccd59ec7cbc82
Nicola-Perera <nicola.perera96@mail.com>
2021-10-28 18:52:17 +0200

    completed bonus
    added v-bind to the source of the image

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,7 @@
 const app = new Vue({
     el:'#root',
     data: {
-        title: "Titolo della pagina"
+        title: "Titolo della pagina",
+        image: "https://picsum.photos/200/300"
     }
 })
